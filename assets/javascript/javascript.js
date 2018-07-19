ea236ecd52d70040e79af91ad8096aefa0514b2b
jpeek01 <jpeek01@gmail.com>
2018-07-19 00:08:57 -0500

    added save topic button that save items to a drop down list. added clear button that clears the gifs

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,7 +4,7 @@ var gifApiReturn = {
     queryURLTypeTrending: "trending",
     queryURLTypeSearch: "search",
     apiKey: "api_key=gOMoKMxetAwahMVySpLTNNM5nOv2LI9R",
-    limit: 2,
+    limit: 25,
     gifArray: {},
 
     getGifs: function(queryURL) {
@@ -18,11 +18,10 @@ var gifApiReturn = {
 
                 for (i = 0; i < gifApiReturn.gifArray.length; i++) {
                     var gifDiv = $("<div class='item'>");
-                    var gif = $("<img>");
+                    var gif = $("<img class='gifs'>");
                     gif.attr("src", gifApiReturn.gifArray[i].images.fixed_height.url)
-                    alert(gifApiReturn.gifArray[i].images.fixed_height.url);
                     gifDiv.append(gif);
-                    $("#gifDisplay").prepend(gifDiv);
+                    $("#gifDisplay").append(gifDiv);
                 }
             });
     },
@@ -36,7 +35,6 @@ var gifApiReturn = {
             gifApiReturn.queryURL = "https://api.giphy.com/v1/gifs/" + queryURLType + "?q=" + searchParameter +
                 "&" + gifApiReturn.apiKey + 
                 "&limit=" + limit;
-                console.log(gifApiReturn.queryURL);
             return gifApiReturn.queryURL;
         }
     }
@@ -45,21 +43,49 @@ var gifApiReturn = {
 
 // object for holding the returned data and associated functions
 var userData = {
-    
-}
+    userTopics: [],
 
+    saveTopic: function(topic) {
+        var topicMenuItem = $("<button class='dropdown-item' type='button'>");
+        $("#topicMenu").append(topicMenuItem);
+        topicMenuItem.text(topic);
+        userData.userTopics.push(topic);
+
+        //logging to the console for troubleshooting purposes
+        console.log("topic " + topic + " saved")
+        userData.userTopics.forEach(element => {
+            console.log(element);
+        });
+    }
+
+
+};
 
 $(document).ready(function() {
-
-    $("#retrieveGifs").click(function() {
+    $("#retrieveGifs").on("click", function() {
+        $("#gifDisplay").empty();
         var searchTerm = $("#searchTerm").val();
         var builtQueryURL = gifApiReturn.buildQueryString(searchTerm,gifApiReturn.queryURLTypeSearch,gifApiReturn.limit);
         gifApiReturn.getGifs(builtQueryURL);
     });
 
-    // var searchTerm = "seinfeld";
-    // var builtQueryURL = gifApiReturn.buildQueryString(searchTerm,gifApiReturn.queryURLTypeSearch,gifApiReturn.limit);
-    // gifApiReturn.getGifs(builtQueryURL);
+    $("#clearGifs").on("click",function() {
+        $("#gifDisplay").empty();
+        $("#searchTerm").val('');
+    });
+
+    $("#saveTopic").on("click",function() {
+        userData.saveTopic($("#searchTerm").val());
+    });
+
+    $("#topicMenu").on("click",function(event) {
+        var menuItemText = $(event.target).text()
+        console.log(menuItemText);
+        $("#searchTerm").val(menuItemText);
+        $("#gifDisplay").empty();
+        var builtQueryURL = gifApiReturn.buildQueryString(menuItemText,gifApiReturn.queryURLTypeSearch,gifApiReturn.limit);
+        gifApiReturn.getGifs(builtQueryURL);
+    });
 });
 
 
